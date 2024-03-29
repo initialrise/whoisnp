4f1a52d3b0e69c22194ef020e74d908a2d7b4722
initialrise <98199146+initialrise@users.noreply.github.com>
2024-03-29 16:15:00 +0545

    Scrap table data logic added

--- controllers/whoisController.js
+++ controllers/whoisController.js
@@ -7,7 +7,6 @@ const grabCSRF = async () => {
   const raw = await session.get("https://register.com.np/whois-lookup");
   const response = raw.data;
   const headers = { headers: { Cookie: raw.headers["set-cookie"] } };
-  console.log(raw.headers["set-cookie"]);
   const $ = cheerio.load(response);
   const token = $('input[type="hidden"]').attr("value");
   return { headers, token };
@@ -31,14 +30,25 @@ const whoisController = async (req, res) => {
       domainExtension: extension,
     });
 
-    console.log("Why not working");
     const raw = await session.post(
       "https://register.com.np/checkdomain_whois",
       body,
       headers
     );
     resp = await raw.data;
-    res.send(resp);
+    const $ = cheerio.load(resp);
+    const whois_table = $("table.table-bordered");
+    const whois_info = {};
+
+    whois_table.find("tr").each((i, element) => {
+      const columns = $(element).find("td");
+      if (columns.length === 2) {
+        const key = columns.eq(0).text().trim();
+        const value = columns.eq(1).text().trim();
+        whois_info[key] = value;
+      }
+    });
+    res.json(whois_info);
   } catch (err) {
     console.error(err);
     res.send(err.message);
